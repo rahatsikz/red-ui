b9762b0fe2a66342238470fc32132b542db029a5
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-02-28 00:48:57 +0600

    [done] ~ checkbox completed

--- src/app/components/checkbox/CheckboxContent.tsx
+++ src/app/components/checkbox/CheckboxContent.tsx
@@ -101,7 +101,7 @@ const CheckboxContent = () => {
     setFontSize(option);
   };
 
-  const [label, setLabel] = useState("Human");
+  const [label, setLabel] = useState("Accept Terms");
 
   const handleLabelChange = (value: any) => {
     setLabel(value);
@@ -142,7 +142,43 @@ const CheckboxContent = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
 
-    setClassNames(``);
+    setClassNames(`
+    <div className='relative flex flex-wrap items-center'>
+      <input
+      className='w-${size} h-${size} cursor-pointer appearance-none rounded border-2 peer focus:outline-none transition-colors border-gray-300 bg-white ${
+      checkboxTypes.class
+    }'
+      type='checkbox'
+      id='one'
+      />
+      <label
+      className="cursor-pointer ${fontSize.class} ${labelColor.text} pl-2"
+      htmlFor='one'
+      >
+      ${label}
+      </label>
+      <svg
+      className="pointer-events-none w-${size} h-${size} absolute ${
+      size === 4 ? (fontSize.label === "Small" ? "2px" : "4px") : "0px"
+    } left-0 -rotate-90 ${animation.class} ${
+      checkboxTypes.svg
+    } opacity-0  peer-checked:rotate-0 peer-checked:opacity-100"
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      aria-hidden='true'
+      aria-labelledby='title-1 description-1'
+      role='graphics-symbol'
+      >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M12.8116 5.17568C12.9322 5.2882 13 5.44079 13 5.5999C13 5.759 12.9322 5.91159 12.8116 6.02412L7.66416 10.8243C7.5435 10.9368 7.37987 11 7.20925 11C7.03864 11 6.87501 10.9368 6.75435 10.8243L4.18062 8.42422C4.06341 8.31105 3.99856 8.15948 4.00002 8.00216C4.00149 7.84483 4.06916 7.69434 4.18846 7.58309C4.30775 7.47184 4.46913 7.40874 4.63784 7.40737C4.80655 7.406 4.96908 7.46648 5.09043 7.57578L7.20925 9.55167L11.9018 5.17568C12.0225 5.06319 12.1861 5 12.3567 5C12.5273 5 12.691 5.06319 12.8116 5.17568Z'
+      />
+      </svg>
+   </div>
+    
+    `);
   };
 
   return (
@@ -226,12 +262,12 @@ const CheckboxContent = () => {
             {/*  */}
             <div className='relative flex flex-wrap items-center'>
               <input
-                className='peer cursor-pointer appearance-none rounded border-2   focus:outline-none transition-colors 
+                className='cursor-pointer appearance-none rounded border-2 peer focus:outline-none transition-colors 
                 border-gray-300 bg-white'
                 type='checkbox'
                 checked={checked}
                 onChange={() => setChecked(!checked)}
-                id='id-c01'
+                id='one'
                 style={{
                   width: size * 4 + "px",
                   height: size * 4 + "px",
@@ -245,7 +281,7 @@ const CheckboxContent = () => {
               />
               <label
                 className={`cursor-pointer ${fontSize.class} pl-2`}
-                htmlFor='id-c01'
+                htmlFor='one'
                 style={{
                   color: labelColor.colorCode,
                 }}

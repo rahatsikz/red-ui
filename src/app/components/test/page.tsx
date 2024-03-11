24dc9e2f129a9b057270adaf64887228fb68a1b6
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-03-11 19:20:58 +0600

    [done] ~ imgbb implemented on card

--- src/app/components/test/page.tsx
+++ src/app/components/test/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Input from "@/Components/ui/Input";
 import React, { useState } from "react";
 
 const TestPage = () => {
@@ -8,8 +9,17 @@ const TestPage = () => {
   };
   return (
     <div className='h-[90vh] flex items-center justify-center bg-red-50 '>
-      <div className='w-1/3 bg-white mx-auto h-[60vh] py-8 px-12'>
+      <div className='w-1/3 bg-white mx-auto h-[60vh] py-8 px-12 flex gap-2'>
         {/* test */}
+
+        <Input
+          name='LabelText'
+          label='Label Text'
+          value={""}
+          type='text'
+          handleChange={() => {}}
+          defaultValue=''
+        />
 
         {/*  */}
       </div>

5a3ee750703d645caace831bf021bb311e3e67dd
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-03-16 18:09:46 +0600

    [done] ~ svg added

--- src/assets/svg/TickMarkSVG.tsx
+++ src/assets/svg/TickMarkSVG.tsx
@@ -8,9 +8,9 @@ const TickMarkSVG = () => {
       width='16'
       height='16'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       fill='none'
     >
       <path d='M5 13l4 4L19 7'></path>

a924d1be2ac6d86a1dbb5330d8f90baa5d207a78
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-03-30 12:45:04 +0600

    [done] ~ toast as a component added

--- src/app/components/[name]/ToastContent.tsx
+++ src/app/components/[name]/ToastContent.tsx
@@ -5,7 +5,7 @@ import Input from "@/Components/ui/Input";
 import Select from "@/Components/ui/Select";
 import React, { useEffect, useState } from "react";
 
-const ToastComponent = ({ message, type, onDismiss }: any) => {
+const ToastComponent = ({ message, type, onDismiss, placement }: any) => {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -14,8 +14,7 @@ const ToastComponent = ({ message, type, onDismiss }: any) => {
       const timer = setTimeout(() => {
         setVisible(false);
         onDismiss();
-      }, 3000);
-
+      }, 2000);
       return () => clearTimeout(timer);
     }
   }, [message, onDismiss]);
@@ -30,11 +29,11 @@ const ToastComponent = ({ message, type, onDismiss }: any) => {
 
   return visible ? (
     <div
-      className={`fixed bottom-16 left-1/2 -translate-x-1/2 z-30 text-white px-4 py-2 rounded-md ${toastStyles[type]}`}
+      className={`fixed ${placement.class} z-30 text-white px-4 py-2 rounded-md ${toastStyles[type]}`}
     >
-      <div className='flex justify-between items-center'>
-        <div className='flex items-center gap-2'>
-          {type === "success" ? (
+      <div className='flex items-center gap-2'>
+        {type === "success" ? (
+          <div className='w-6'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               height='24'
@@ -44,7 +43,9 @@ const ToastComponent = ({ message, type, onDismiss }: any) => {
             >
               <path d='m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
             </svg>
-          ) : type === "error" ? (
+          </div>
+        ) : type === "error" ? (
+          <div className='w-6'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               height='24'
@@ -54,7 +55,9 @@ const ToastComponent = ({ message, type, onDismiss }: any) => {
             >
               <path d='m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
             </svg>
-          ) : (
+          </div>
+        ) : (
+          <div className='w-6'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               height='24'
@@ -64,9 +67,9 @@ const ToastComponent = ({ message, type, onDismiss }: any) => {
             >
               <path d='M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
             </svg>
-          )}
-          <p className='text-sm'>{message}</p>
-        </div>
+          </div>
+        )}
+        <p className='text-sm text-center'>{message}</p>
       </div>
     </div>
   ) : null;
@@ -101,6 +104,32 @@ const ToastContent = () => {
 
   const handleMessageChange = (value: any) => {
     setMessage(value);
+  };
+
+  const toastPlacementOptions = [
+    {
+      label: "Top",
+      value: "top",
+      class: "top-16 left-1/2 -translate-x-1/2 ",
+    },
+    {
+      label: "Bottom",
+      value: "bottom",
+      class: "bottom-16 left-1/2 -translate-x-1/2",
+    },
+    {
+      label: "Right",
+      value: "right",
+      class: "right-4 lg:right-16 bottom-16",
+    },
+  ];
+
+  const [toastPlacement, setToastPlacement] = useState<any>(
+    toastPlacementOptions[0]
+  );
+
+  const handleToastPlacementChange = (value: any) => {
+    setToastPlacement(value);
   };
 
   const [toastQueue, setToastQueue] = useState<any>([]);
@@ -148,7 +177,214 @@ const ToastContent = () => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
+    setCode(`
+import React, { useEffect, useState } from "react";
+
+type ToastProps = {
+  message: string;
+  type: "success" | "warning" | "error";
+  onDismiss: () => void;
+};
+    
+const ToastComponent = ({ message, type, onDismiss }: ToastProps) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (message) {
+      setVisible(true);
+      const timer = setTimeout(() => {
+        setVisible(false);
+        onDismiss();
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [message, onDismiss]);
+
+  const toastStyles: any = {
+    success: "bg-green-500",
+    error: "bg-red-400",
+    warning: "bg-orange-300",
   };
+    
+  return visible ? (
+    <div className={"fixed ${toastPlacement.class} z-30 text-white px-4 py-2 rounded-md " + toastStyles[type]}>
+      <div className='flex items-center gap-2'>
+        {type === "success" ? (
+          <div className='w-6'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24'
+              viewBox='0 -960 960 960'
+              width='24'
+              fill='white'
+            >
+              <path d='m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
+            </svg>
+          </div>
+        ) : type === "error" ? (
+          <div className='w-6'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24'
+              viewBox='0 -960 960 960'
+              width='24'
+              fill='white'
+            >
+              <path d='m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
+            </svg>
+          </div>
+        ) : (
+          <div className='w-6'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24'
+              viewBox='0 -960 960 960'
+              width='24'
+              fill='white'
+            >
+              <path d='M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
+            </svg>
+          </div>
+        )}
+        <p className='text-sm text-center'>{message}</p>
+      </div>
+    </div>
+  ) : null;
+};
+    
+    export default ToastComponent;
+    
+    `);
+
+    if (toastType.value !== "warning") {
+      setComponentCode(`
+const HowToUseToast = () => {
+  const [toastQueue, setToastQueue] = useState<any>([]);
+
+  const handleShowToast = (message: string, type = "success") => {
+    setToastQueue((prevQueue: any) => [...prevQueue, { message, type }]);
+  };
+
+  const handleAdd = () => {
+    fetch(
+      "https://jsonplaceholder.typicode.com/${
+        toastType.value === "success" ? "posts" : "nonexistent-endpoint"
+      }", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to create post");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        handleShowToast("${message}", "success");
+      })
+      .catch((err) => {
+        console.log(err);
+          handleShowToast("${message}", "error");
+      });
+  };
+  return (
+    <>
+      {toastQueue.length > 0 && (
+        <ToastComponent
+          message={toastQueue[0].message}
+          type={toastQueue[0].type}
+          onDismiss={() =>
+            setToastQueue((prevQueue: any) => prevQueue.slice(1))
+          }
+        />
+      )}
+
+      <button
+        onClick={handleAdd}
+        className='rounded-full px-[26px] py-[6px] bg-gray-900 text-white'
+      >
+        Click to Show Toast
+      </button>
+    </>
+  );
+};
+    `);
+    } else {
+      setComponentCode(`
+const HowToUseToast = () => {
+  const [toastQueue, setToastQueue] = useState<any>([]);
+
+  const handleShowToast = (message: string, type = "success") => {
+    setToastQueue((prevQueue: any) => [...prevQueue, { message, type }]);
+  };
+
+  const handleAdd = () => {
+    fetch(
+      "https://jsonplaceholder.typicode.com/${
+        toastType.value === "success" ? "posts" : "nonexistent-endpoint"
+      }", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to create post");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        handleShowToast("${message}", "success");
+      })
+      .catch((err) => {
+        console.log(err);
+          handleShowToast("${message}", "warning");
+      });
+  };
+  return (
+    <>
+      {toastQueue.length > 0 && (
+        <ToastComponent
+          message={toastQueue[0].message}
+          type={toastQueue[0].type}
+          onDismiss={() =>
+            setToastQueue((prevQueue: any) => prevQueue.slice(1))
+          }
+        />
+      )}
+
+      <button
+        onClick={handleAdd}
+        className='rounded-full px-[26px] py-[6px] bg-gray-900 text-white'
+      >
+        Click to Show Toast
+      </button>
+    </>
+  );
+};
+    `);
+    }
+  };
+
+  useEffect(() => {
+    setCode("");
+  }, [toastType, message, toastPlacement]);
+
   return (
     <section>
       <div className='xl:grid grid-cols-12 flex flex-col gap-16 lg:h-[calc(100vh-5.6rem)] h-full'>
@@ -171,6 +407,12 @@ const ToastContent = () => {
                 type='text'
                 handleChange={handleMessageChange}
                 defaultValue=''
+              />
+              <Select
+                label='Toast Placement'
+                options={toastPlacementOptions}
+                onChange={handleToastPlacementChange}
+                value={toastPlacement}
               />
             </div>
             <div className='mt-8 flex justify-center'>
@@ -197,6 +439,7 @@ const ToastContent = () => {
               <ToastComponent
                 message={toastQueue[0].message}
                 type={toastQueue[0].type}
+                placement={toastPlacement}
                 onDismiss={() =>
                   setToastQueue((prevQueue: any) => prevQueue.slice(1))
                 }
